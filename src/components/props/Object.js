633a008b56394606f0311af6905cada16038c01b
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-09 17:34:16 +0530

    Object.js

--- src/components/props/Object.js
+++ src/components/props/Object.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Object = (props) => {
     return (
-        <div className=' bg-red-500 w-fit text-white'>
+        <div className='text-xl'>
         {<p>employee: {props.employee.empname}</p>}
         </div>
     )
